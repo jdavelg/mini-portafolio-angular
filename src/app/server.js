2dbcf30350d0e6a2dc2019b1709e45a3b797db75
jonathan lopez <jdavelg@gmail.com>
2020-07-16 22:00:44 -0600

    server al dia

--- src/app/server.js
+++ src/app/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static('./dist/blog-angular'));
 app.get('/*',function(req,res){
-    res.sendFile('index.html', {root: 'dist/blog-angular/'}
+    res.sendFile('index.html', {root: 'dist/proyecto-angular/'}
     );
 });
 
