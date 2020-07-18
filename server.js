ded5576da296fdb722acb08d51b5d402120e79d4
jonathan lopez <jdavelg@gmail.com>
2020-07-18 01:50:30 -0600

    server addes to main hall of app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.static('./dist/blog-angular'));
+app.get('/*',function(req,res){
+    res.sendFile('index.html', {root: 'dist/proyecto-angular/'}
+    );
+});
+
+app.listen(process.env.PORT || 8080);
