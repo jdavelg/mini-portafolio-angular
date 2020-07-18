f831d80f344e593b3d7e42ebbaf7ceec10c84de7
jonathan lopez <jdavelg@gmail.com>
2020-07-18 02:00:00 -0600

    servers jss fixeds

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/blog-angular'));
+app.use(express.static('./dist/proyecto-angular'));
 app.get('/*',function(req,res){
     res.sendFile('index.html', {root: 'dist/proyecto-angular/'}
     );
